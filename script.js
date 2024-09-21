249244178b469a0289f978762ec3a07dd19d9cb8
simokhin <nick.simokhin@yandex.ru>
2024-09-21 09:35:33 +0300

    Add function that get human choice

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 function getComputerChoice() {
-    a = Math.floor(Math.random() * 10);
-    if (a <= 3) {
-        return "Rock";
+    a = Math.floor(Math.random() * 100);
+    if (a <= 33) {
+        return "rock";
     }
-    else if (a <= 7) {
-        return "Paper";
+    else if (a <= 66) {
+        return "paper";
     }
     else {
-        return "Scissors";
+        return "scissors";
     }
 }
 
 console.log(getComputerChoice());
+
+function getHumanChoice() {
+    let choice = prompt("What is your choice?").toLowerCase();
+    if (choice === "rock") {
+        return "rock";
+    }
+    else if (choice === "paper") {
+        return "paper";
+    }
+    else if (choice === "scissors") {
+        return "scissors";
+    }
+    else {
+        return "Try again.";
+    }
+}
+
+console.log(getHumanChoice())
+
