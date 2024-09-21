45c0891a398985eedc131eaf6b135922c2a93515
simokhin <nick.simokhin@yandex.ru>
2024-09-21 11:55:06 +0300

    Fix playRound function

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 let humanScore = 0;
 let computerScore = 0;
 
+let humanSelect = getHumanChoice();
+let computerSelect = getComputerChoice(); 
+
+function playGame() {
+    playRound
+}
+
+function playGame() {
+    if (humanScore >= 5 && computerScore < 5){
+        return console.log("You win the game");
+    }
+    else if (computerScore >= 5 && humanScore < 5){
+        return console.log("You lost the game.")
+    }
+    else {
+        let round = playRound(humanSelect, computerSelect);
+            if (round === "You win.") {
+                humanScore = humanScore + 1;
+                return console.log(humanScore, computerScore)
+            }
+            else if (round === "You lost.") {
+                computerScore = computerScore +1;
+                return console.log(humanScore, computerScore);
+            }
+            else {
+                return console.log(humanScore, computerScore);
+            }
+    }
+}
+
 function getComputerChoice() {
     a = Math.floor(Math.random() * 100);
     if (a <= 33) {
@@ -14,8 +44,6 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
-
 function getHumanChoice() {
     let choice = prompt("What is your choice?").toLowerCase();
     if (choice === "rock") {
@@ -27,58 +55,52 @@ function getHumanChoice() {
     else if (choice === "scissors") {
         return "scissors";
     }
+    else if (choice === none) {
+        return "Try again.";
+    }
     else {
         return "Try again.";
     }
 }
 
-console.log(getHumanChoice())
-
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === "rock") {
         if (computerChoice === "scissors") {
-            ++humanScore;
+            humanScore = humanScore + 1;
             return console.log("You win.")
         }
         else if (computerChoice === "rock") {
             return console.log("Draw.")
         }
         else {
-            ++computerScore;
+            computerScore = computerScore + 1;
             return console.log("You lost.")
         }
     }
     else if (humanChoice === "paper") {
         if (computerChoice === "scissors") {
-            ++computerChoice;
+            computerScore = computerScore + 1;
             return console.log("You lost.")
         }
         else if (computerChoice === "paper") {
             return console.log("Draw.")
         }
         else {
-            ++humanScore;
+            humanScore = humanScore + 1;
             return console.log("You win.")
         }
     }
     else {
         if (computerChoice === "paper") {
-            ++humanScore;
+            humanScore = humanScore + 1;
             return console.log("You win.")
         }
         else if (computerChoice === "scissors") {
             return console.log("Draw.")
         }
         else {
-            ++computerChoice;
+            computerScore = computerScore + 1;
             return console.log("You lost.")
         }
     }
-}
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-function playGame() {
-    
 }
