ff706d32face30d1073863aad578db09ae817f26
simokhin <nick.simokhin@yandex.ru>
2024-09-21 10:26:28 +0300

    Add function that plays 1 round and score

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     a = Math.floor(Math.random() * 100);
     if (a <= 33) {
@@ -31,3 +34,51 @@ function getHumanChoice() {
 
 console.log(getHumanChoice())
 
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock") {
+        if (computerChoice === "scissors") {
+            ++humanScore;
+            return console.log("You win.")
+        }
+        else if (computerChoice === "rock") {
+            return console.log("Draw.")
+        }
+        else {
+            ++computerScore;
+            return console.log("You lost.")
+        }
+    }
+    else if (humanChoice === "paper") {
+        if (computerChoice === "scissors") {
+            ++computerChoice;
+            return console.log("You lost.")
+        }
+        else if (computerChoice === "paper") {
+            return console.log("Draw.")
+        }
+        else {
+            ++humanScore;
+            return console.log("You win.")
+        }
+    }
+    else {
+        if (computerChoice === "paper") {
+            ++humanScore;
+            return console.log("You win.")
+        }
+        else if (computerChoice === "scissors") {
+            return console.log("Draw.")
+        }
+        else {
+            ++computerChoice;
+            return console.log("You lost.")
+        }
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+function playGame() {
+    
+}
