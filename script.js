97825ac53acaf3053293c374347d07b9f6cc56b5
simokhin <nick.simokhin@yandex.ru>
2024-09-22 17:40:11 +0300

    Add buttons that play a round when clicked
    The results are displayed in the paragraph

--- script.js
+++ script.js
@@ -1,40 +1,31 @@
 let humanScore = 0;
 let computerScore = 0;
 
-let humanSelect = getHumanChoice();
-let computerSelect = getComputerChoice(); 
+let btnRock = document.querySelector("#rock");
+let btnPaper = document.querySelector("#paper");
+let btnScissors = document.querySelector("#scissors");
 
-playGame();
-
-function playGame() {
+btnRock.addEventListener("click", () => {
+    humanSelect = "Rock";
+    computerSelect = getComputerChoice(); 
     playRound(humanSelect, computerSelect);
+});
 
-    humanSelect = getHumanChoice();
+btnPaper.addEventListener("click", () => {
+    humanSelect = "Paper";
     computerSelect = getComputerChoice(); 
     playRound(humanSelect, computerSelect);
 
-    humanSelect = getHumanChoice();
+});
+
+btnScissors.addEventListener("click", () => {
+    humanSelect = "Scissors";
     computerSelect = getComputerChoice(); 
     playRound(humanSelect, computerSelect);
+});
 
-    humanSelect = getHumanChoice();
-    computerSelect = getComputerChoice(); 
-    playRound(humanSelect, computerSelect);
-
-    humanSelect = getHumanChoice();
-    computerSelect = getComputerChoice(); 
-    playRound(humanSelect, computerSelect);
-
-    if (humanScore > computerScore) {
-        return console.log("You win the game! (Ты победил!)");
-    }
-    else if (computerScore > humanScore) {
-        return console.log("You lost the game. (Ты проиграл игру...)");
-    }
-    else {
-        return console.log("The game ended in a draw. (Игра закончилась ничьей)")
-    }
-}
+let score = document.querySelector("#score");
+score.textContent = `You: ${humanScore} Computer: ${computerScore}`
 
 function getComputerChoice() {
     number = Math.random();
@@ -49,62 +40,54 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let choice = prompt("What is your choice? (Rock, Paper, Scissors) (Выбирай: камень, ножницы или бумага)").toLowerCase();
-    if (choice === "rock" || choice === "камень") {
-        return "rock";
-    }
-    else if (choice === "paper" || choice === "бумага") {
-        return "paper";
-    }
-    else if (choice === "scissors" || choice === "ножницы") {
-        return "scissors";
-    }
-    else if (choice === none) {
-        return "Try again. (Попробуй ещё раз)";
-    }
-    else {
-        return "Try again. (Попробуй ещё раз)";
-    }
-}
 
 function playRound(humanChoice, computerChoice) {
+    let roundResult = document.querySelector("#round-result");
     if (humanChoice === "rock") {
         if (computerChoice === "scissors") {
             humanScore = humanScore + 1;
+            roundResult.textContent = "You win. (Ты победил в этом раунде)";
             return console.log("You win. (Ты победил в этом раунде)");
         }
         else if (computerChoice === "rock") {
+            roundResult.textContent = "Draw. (Ничья в этом раунде)";
             return console.log("Draw. (Ничья в этом раунде)");
         }
         else {
             computerScore = computerScore + 1;
+            roundResult.textContent = "You lost. (Ты проиграл в этом раунде)";
             return console.log("You lost. (Ты проиграл в этом раунде)");
         }
     }
     else if (humanChoice === "paper") {
         if (computerChoice === "scissors") {
             computerScore = computerScore + 1;
+            roundResult.textContent = "You lost. (Ты проиграл в этом раунде)";
             return console.log("You lost. (Ты проиграл в этом раунде)");
         }
         else if (computerChoice === "paper") {
+            roundResult.textContent = "Draw. (Ничья в этом раунде)";
             return console.log("Draw. (Ничья в этом раунде)");
         }
         else {
             humanScore = humanScore + 1;
+            roundResult.textContent = "You win. (Ты победил в этом раунде)";
             return console.log("You win. (Ты победил в этом раунде)");
         }
     }
     else {
         if (computerChoice === "paper") {
             humanScore = humanScore + 1;
+            roundResult.textContent = "You win. (Ты победил в этом раунде)";
             return console.log("You win. (Ты победил в этом раунде)");
         }
         else if (computerChoice === "scissors") {
+            roundResult.textContent = "Draw. (Ничья в этом раунде)";
             return console.log("Draw. (Ничья в этом раунде)");
         }
         else {
             computerScore = computerScore + 1;
+            roundResult.textContent = "You lost. (Ты проиграл в этом раунде)";
             return console.log("You lost. (Ты проиграл в этом раунде)");
         }
     }
